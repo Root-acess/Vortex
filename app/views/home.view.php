c60dfd6927a4a2d8bb89ddc2b2fb5124c5d83bf7
hiralal <hiralals221@gmail.com>
2024-05-22 11:13:33 +0530

    add everything

---
+++ app/views/home.view.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vortex</title>
+    <link rel="stylesheet" href="public/css/index.css">
+ 
+</head>
+<body>
+
+    <div>
+        <h1>Welcome to Vortex</h1>
+        <h2>where PHP development takes flight</h2>
+        <img src="public/images/vortex.svg" alt="">
+    </div>
+</body>
+</html>
+
